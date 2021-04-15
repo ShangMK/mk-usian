bc40f9e16a364e91e07928a116919874458f8bdf
MK <2191158600@qq.com>
2021-04-15 19:30:37 +0800

    4/15

---
+++ usian-person-itemcategory-service/src/main/java/com/usian/controller/SerchItemControllers.java
@@ -0,0 +1,23 @@
+package com.usian.controller;
+
+import com.usian.service.ItemCategoryServices;
+import com.usian.service.SeachItemService;
+import com.usian.utils.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RequestMapping("seachservice")
+@RestController
+public class SerchItemControllers {
+
+    @Autowired
+    SeachItemService seachItemService;
+
+    @RequestMapping("list")
+    public ArrayList<Object> list(String q){
+        return seachItemService.list(q);
+    };
+}
